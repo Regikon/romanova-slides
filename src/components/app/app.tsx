92696bfb666c7b260ca2e98f0a66a3b1906d16dd
Ilya Andriyanov <ilyaandry35@gmail.com>
2025-03-13 14:35:51 +0300

    Publishing to gh-pages (publish.sh)

--- src/components/app/app.tsx
+++
@@ -1,9 +0,0 @@
-import LogisticRegressionPresentation from "../logistic_regression_presentation/logistic_regression_presentation";
-
-function App() {
-	return (
-		<LogisticRegressionPresentation />
-	);
-}
-
-export default App;
