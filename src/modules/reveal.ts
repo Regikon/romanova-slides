42e4e64425eff8138782ae13dcf6fe3318b85ccd
Ilya Andriyanov <ilyaandry35@gmail.com>
2025-03-13 14:50:16 +0300

    fix: katex

--- src/modules/reveal.ts
+++ src/modules/reveal.ts
@@ -5,32 +5,31 @@ import RevealMath from 'reveal.js/plugin/math/math';
 
 // generateRef generates react ref to be binded to presentation
 export const generateRef = () => {
-	const deckDivRef = useRef<HTMLDivElement>(null);
-	const deckRef = useRef<Reveal.Api | null>(null);
+    const deckDivRef = useRef<HTMLDivElement>(null);
+    const deckRef = useRef<Reveal.Api | null>(null);
 
-	useEffect(() => {
-		// Prevents double init in strict
-		if (deckRef.current) return;
+    useEffect(() => {
+        // Prevents double init in strict
+        if (deckRef.current) return;
 
-		deckRef.current = new Reveal(deckDivRef.current!, {
-			plugins: [RevealNotes, RevealMath.KaTeX],
-			transition: 'slide',
-			katex: { local: 'node_modules/katex' },
-		});
+        deckRef.current = new Reveal(deckDivRef.current!, {
+            plugins: [RevealNotes, RevealMath.KaTeX],
+            transition: 'slide',
+        });
 
-		deckRef.current.initialize();
+        deckRef.current.initialize();
 
-		return () => {
-			try {
-				if (deckRef.current) {
-					deckRef.current.destroy();
-					deckRef.current = null;
-				}
-			} catch (e) {
-				console.warn("Reveal.js destroy call failed.");
-			}
-		};
-	}, []);
-	return deckDivRef;
+        return () => {
+            try {
+                if (deckRef.current) {
+                    deckRef.current.destroy();
+                    deckRef.current = null;
+                }
+            } catch (e) {
+                console.warn("Reveal.js destroy call failed.");
+            }
+        };
+    }, []);
+    return deckDivRef;
 }
 
