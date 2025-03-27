828edb5e0a5090765b5fb38ab2f4dffd92c9fe79
Ilya Andriyanov <ilyaandry35@gmail.com>
2025-03-27 11:29:03 +0300

    fix: router

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,11 @@
 import LogisticRegressionPresentation from "../logistic_regression_presentation/logistic_regression_presentation";
 import RecurrentNeuralNetwork from "../recurrent_neural_network_presentation/recurrent_neural_network";
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { HashRouter, Routes, Route } from 'react-router'
 import TheBestRoutingPage from "../the_best_routing_page/the_best_routing_pate";
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="romanova-slides" >
                     <Route index element={<TheBestRoutingPage />} />
@@ -13,7 +13,7 @@ function App() {
                     <Route path="rnn" element={<RecurrentNeuralNetwork />} />
                 </Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
